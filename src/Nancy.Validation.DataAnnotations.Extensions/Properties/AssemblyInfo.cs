66befa9acccf8aa3daf1b666dfb8e06633864767
Brian Surowiec <xtorted@optonline.net>
2015-01-11 05:29:14 -0500

    Add shared assembly info

--- src/Nancy.Validation.DataAnnotations.Extensions/Properties/AssemblyInfo.cs
+++ src/Nancy.Validation.DataAnnotations.Extensions/Properties/AssemblyInfo.cs
@@ -2,12 +2,3 @@
 
 [assembly: AssemblyTitle("Nancy.Validation.DataAnnotations.Extensions")]
 [assembly: AssemblyDescription("")]
-
-[assembly: AssemblyCompany("Brian Surowiec")]
-[assembly: AssemblyProduct("Nancy.Validation.DataAnnotations.Extensions")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
-
-[assembly: AssemblyInformationalVersion("0.1.0")]
-
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
--- test/Nancy.Validation.DataAnnotations.Extensions.Tests/Properties/AssemblyInfo.cs
+++ test/Nancy.Validation.DataAnnotations.Extensions.Tests/Properties/AssemblyInfo.cs
@@ -2,12 +2,3 @@
 
 [assembly: AssemblyTitle("Nancy.Validation.DataAnnotations.Extensions.Tests")]
 [assembly: AssemblyDescription("")]
-
-[assembly: AssemblyCompany("Brian Surowiec")]
-[assembly: AssemblyProduct("Nancy.Validation.DataAnnotations.Extensions.Tests")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
-
-[assembly: AssemblyInformationalVersion("0.1.0")]
-
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
