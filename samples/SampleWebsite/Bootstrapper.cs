a2d397977c56abda251047b997efa2ea140116ef
Brian Surowiec <xtorted@optonline.net>
2015-01-13 19:22:52 -0500

    Add sample showing the attributes in use

---
+++ samples/SampleWebsite/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿namespace SampleWebsite
+{
+    using Nancy.Conventions;
+
+    public class Bootstrapper : Nancy.DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions(conventions);
+
+            conventions.StaticContentsConventions.AddDirectory("Scripts");
+        }
+    }
+}
