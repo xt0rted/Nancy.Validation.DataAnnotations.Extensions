802047f0e48a66256cc56b450adf117751e97472
Brian Surowiec <xtorted@optonline.net>
2015-05-06 14:50:37 -0400

    Update nancy to v1.2.0

--- src/Nancy.Validation.DataAnnotations.Extensions/Properties/AssemblyInfo.cs
+++ src/Nancy.Validation.DataAnnotations.Extensions/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
 ﻿using System.Reflection;
 
+using Nancy;
+
 [assembly: AssemblyTitle("Nancy.Validation.DataAnnotations.Extensions")]
 [assembly: AssemblyDescription(".NET 4.5 Data Annotation support for Nancy")]
+
+[assembly: IncludeInNancyAssemblyScanning]
