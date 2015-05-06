ef241fe7ee98ebbde66b0ab34fbdd122e6a81fe2
Brian Surowiec <xtorted@optonline.net>
2015-05-06 15:33:07 -0400

    Bumping the version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@
 [assembly: AssemblyProduct("Nancy.Validation.DataAnnotations.Extensions.Tests")]
 [assembly: AssemblyCopyright("Copyright © 2015")]
 
-[assembly: AssemblyInformationalVersion("0.3.0")]
+[assembly: AssemblyInformationalVersion("0.4.0")]
 
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.3.0")]
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyFileVersion("0.4.0")]
